c52f1ab0bc1726c3a7b9df6baeef1b70cac1bdde
Helge Hafting <helge.hafting@ntnu.no>
2023-10-16 19:45:33 +0200

    Initial checkin. Ruleset and tileset with 3 extra hill types.

---
+++ hh/README.hh
@@ -0,0 +1,7 @@
+Purposes of hh ruleset:
+
+1. Combine "Units see longer from mountains" from civ2civ3, with "City reach depends on its size" from sandbox. So both these rules works in the same game.
+(Done)
+
+2. Test games with extra terrain types. This requires support from the ruleset. Extra terrain types gives nicer terrains.
+
